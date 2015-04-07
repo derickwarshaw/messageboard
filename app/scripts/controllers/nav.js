8ce1f44e4b1c8e3951806cc2ef27925ed131a854
Derick Warshaw <derickwarshaw@gmail.com>
2015-04-07 19:12:37 -0400

    initial

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,27 @@
+'use strict';
+
+app.controller('NavCtrl', function ($scope, $location, Post, Auth) {
+    $scope.post = {
+        url: 'http://',
+        title: ''
+    };
+    $scope.signedIn = Auth.signedIn;
+    $scope.logout = Auth.logout;
+    console.log($scope.user);
+
+
+    // console.log($scope.username.profile);
+
+    $scope.submitPost = function() {
+        $scope.post.creator = $scope.user.profile.username;
+        $scope.post.creatorUID = $scope.user.uid;
+        Post.create($scope.post).then(function(ref) {
+            $location.path('/posts/' + ref.name());
+            $scope.post = {
+                url: 'http://',
+                title: ''
+            };
+        });
+    };
+
+});
---
+++ app/scripts/controllers/posts.js
@@ -0,0 +1,15 @@
+'use strict'
+
+app.controller('PostsCtrl', function ($scope, Post, Auth) {
+  $scope.posts = Post.all;
+  $scope.user = Auth.user;
+
+  $scope.post = {url: 'http://', 'title': ''};
+
+  $scope.deletePost = function (post) {
+    Post.delete(post);
+  };
+
+
+
+});
